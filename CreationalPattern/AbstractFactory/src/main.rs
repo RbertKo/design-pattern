4ff4582562690c9c4e3d2ecf968f8f620f1ae86d
R.bertKo <myeongsku@gmail.com>
2020-03-29 10:50:48 +0900

    Finish AstractFactory Paatern

--- CreationalPattern/AbstractFactory/src/main.rs
+++ CreationalPattern/AbstractFactory/src/main.rs
@@ -1,3 +1,59 @@
+/*  
+*   Core
+*/
+
+trait Image {
+    fn get_image(&self) -> String;
+}
+
+trait Factory<P: Image> {
+    fn create_container(&self) -> P;
+}
+
+/*  
+*   TensorflowContainer
+*/
+
+struct TensorflowImage;
+struct TensorflowContainer;
+
+impl Image for TensorflowImage {
+    fn get_image(&self) -> String {
+        return String::from("tensorflow/tensorflow");
+    }
+}
+
+impl Factory<TensorflowImage> for TensorflowContainer {
+    fn create_container(&self) -> TensorflowImage {
+        println!("Success : {}", TensorflowImage.get_image());
+        return TensorflowImage;
+    }
+}
+
+/*  
+*   RContainer
+*/
+
+struct RImage;
+struct RContainer;
+
+impl Image for RImage {
+    fn get_image(&self) -> String {
+        return String::from("tensorflow/tensorflow");
+    }
+}
+
+impl Factory<RImage> for RContainer {
+    fn create_container(&self) -> RImage {
+        println!("Success : {}", RImage.get_image());
+        return RImage;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tensorflow = TensorflowContainer;
+    let r = RContainer;
+
+    tensorflow.create_container();
+    r.create_container();
 }

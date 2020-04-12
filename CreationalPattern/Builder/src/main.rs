c3ab52ccbc008090a413d0c347a44b4578f47181
R.bertKo <myeongsku@gmail.com>
2020-04-13 04:26:15 +0900

    Rust test

--- CreationalPattern/Builder/src/main.rs
+++ CreationalPattern/Builder/src/main.rs
@@ -1,3 +1,16 @@
+struct Builder {
+
+}
+
+impl Builder {
+    fn build(&self) -> &String {
+        let return_val = String::from("test");
+        return return_val;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let instance = Builder {};
+
+    println!("{}", instance.build());
 }

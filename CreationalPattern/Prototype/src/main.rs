336c2b1215c4de055d0f74af45e2786b7b88feae
R.bertKo <myeongsku@gmail.com>
2020-04-21 08:23:12 +0900

    Create Circle class

--- CreationalPattern/Prototype/src/main.rs
+++ CreationalPattern/Prototype/src/main.rs
@@ -1,3 +1,19 @@
+struct Coordination(u8, u8);
+
+struct Circle {
+    center: Coordination,
+}
+
+impl Circle {
+    fn new(center: Coordination) -> Self {
+        Self {
+            center
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_circle = Circle::new(Coordination(0, 0));
+
+    println!("my center coordination X : {}   Y : {}", my_circle.center.0, my_circle.center.1);
 }

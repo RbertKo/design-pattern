1b1f9afac4cd4d3909d6c2a4298135ef3305221a
R.bertKo <myeongsku@gmail.com>
2020-04-28 23:56:20 +0900

    INIT : readme

--- CreationalPattern/Singlton/src/main.rs
+++ CreationalPattern/Singlton/src/main.rs
@@ -1,9 +1,19 @@
 use std::collections::HashMap;
 
 struct DB {
-    das: 
+    dao: HashMap<u16, String>
+    instance_existed : bool
+}
+
+impl DB {
+    fn get_instance() -> Self {
+        Self {
+            dao: HashMap::new(),
+            instance_existed :: true
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    
 }
